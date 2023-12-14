10e1bce387cbd2c2eb8f64a30b6faa5167a75ae3
wlutz <lutz0willy@gmail.com>
2023-12-14 17:24:38 +0100

    update of fiiireflyyy

--- scripts/pipelines.py
+++ scripts/pipelines.py
@@ -10,93 +10,82 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 
 
-def confusion(train, test, merge_path="example_data/merge.csv"):
+def confusion(train, test=None, merge_path="example_data/merge.csv", savepath='', title=''):
     percentiles = 0.1
     
-    
-    
-    merge = pd.read_csv(os.path.join(os.getcwd(), merge_path), index_col=False)
+    merge = pd.read_csv(os.path.join(os.getcwd(), merge_path))
     
     # random split train test for train labels
-    df_train = merge[merge["label"].isin(train)]
-    X = df_train[df_train.columns[:-1]]
-    y = df_train["label"]
-    X_train_tr, X_test_tr, y_train_tr, y_test_tr = train_test_split(X, y, train_size=0.7)
+    df_train_labels = merge[merge["label"].isin(train)]
+    X = df_train_labels[df_train_labels.columns[:-1]]
+    y = df_train_labels["label"]
+    X_tr_train, X_te_train, y_tr_train, y_te_train = train_test_split(X, y, train_size=0.5)
     
-    # random split train test for test labels
-    # todo : handle case where there is not test
-    df_test = merge[merge["label"].isin(test)]
-    X = df_test[df_test.columns[:-1]]
-    y = df_test["label"]
-    X_train_te, X_test_te, y_train_te, y_test_te = train_test_split(X, y, train_size=0.7)
+    X_tr_train["label"] = y_tr_train
+    X_te_train["label"] = y_te_train
     
-    # aggregating label column for outlier removal
-    X_train_tr["label"] = y_train_tr
-    X_train_te["label"] = y_train_te
-    X_test_tr["label"] = y_test_tr
-    X_test_te["label"] = y_test_te
-    
-    # get iqr metrics on training labels only
-    _, iqr_metrics = fp.discard_outliers_by_iqr(X_train_tr, low_percentile=percentiles,
+    # discarding outliers
+    _, iqr_metrics = fp.discard_outliers_by_iqr(X_tr_train, low_percentile=percentiles,
                                                 high_percentile=1 - percentiles,
                                                 mode='capping', metrics=None)
-    
-    # apply outlier removal to all sub datasets
-    X_train_tr = fp.discard_outliers_by_iqr(X_train_tr, low_percentile=percentiles,
+    X_tr_train = fp.discard_outliers_by_iqr(X_tr_train, low_percentile=percentiles,
                                             high_percentile=1 - percentiles,
                                             mode='capping', metrics=iqr_metrics)
-    X_train_te = fp.discard_outliers_by_iqr(X_train_te, low_percentile=percentiles,
+    X_te_train = fp.discard_outliers_by_iqr(X_te_train, low_percentile=percentiles,
                                             high_percentile=1 - percentiles,
                                             mode='capping', metrics=iqr_metrics)
-    X_test_tr = fp.discard_outliers_by_iqr(X_test_tr, low_percentile=percentiles,
-                                           high_percentile=1 - percentiles,
-                                           mode='capping', metrics=iqr_metrics)
-    X_test_te = fp.discard_outliers_by_iqr(X_test_te, low_percentile=percentiles,
-                                           high_percentile=1 - percentiles,
-                                           mode='capping', metrics=iqr_metrics)
+    # random split train test for test labels
+    df_test_labels = pd.DataFrame()
+    if test:
+        df_test_labels = merge[merge["label"].isin(test)]
+        df_test_labels = fp.discard_outliers_by_iqr(df_test_labels, low_percentile=percentiles,
+                                                    high_percentile=1 - percentiles,
+                                                    mode='capping', metrics=iqr_metrics)
     
-    # concatenating post outliers removal for model training
-    processed_df_train = pd.concat([X_train_tr, X_test_tr], ignore_index=True)
-    processed_df_test = pd.concat([X_train_te, X_test_te], ignore_index=True)
+    X_tr_train = X_tr_train[X_tr_train.columns[:-1]]
+    rfc = fl.train_RFC_from_dataset(X_tr_train, y_tr_train)
     
-    
-    
-    rfc, _ = fl.train_RFC_from_dataset(processed_df_train)
-    fl.test_clf_by_confusion(rfc, pd.concat([processed_df_train, processed_df_test], ignore_index=True),
+    df = pd.concat([X_te_train, df_test_labels], ignore_index=True)
+    fl.test_clf_by_confusion(rfc, df,
                              training_targets=train,
                              testing_targets=train + test,
-                             show=True, verbose=False, savepath="",
-                             title=f"",
+                             show=True,
+                             verbose=False,
+                             savepath=savepath,
+                             title=title,
                              iterations=10, )
 
 
-def pca(train, test, n_component, merge_path):
+def pca(train, test=None, merge_path="example_data/merge.csv", savepath='', title=''):
     percentiles = 0.1
+    merge = pd.read_csv(os.path.join(os.getcwd(), merge_path))
     
-    merge = pd.read_csv(os.path.join(os.getcwd(), merge_path), index_col=False)
-    train_dataframes = []
-    test_dataframes = []
+    # random split train test for train labels
+    df_train_labels = merge[merge["label"].isin(train)]
+    # discarding outliers
+    _, iqr_metrics = fp.discard_outliers_by_iqr(df_train_labels, low_percentile=percentiles,
+                                                high_percentile=1 - percentiles,
+                                                mode='capping', metrics=None)
+    df_train_labels = fp.discard_outliers_by_iqr(df_train_labels, low_percentile=percentiles,
+                                                 high_percentile=1 - percentiles,
+                                                 mode='capping', metrics=iqr_metrics)
     
-    for tr in train:
-        df = merge[merge["label"] == tr]
-        df, _ = fp.discard_outliers_by_iqr(df, low_percentile=percentiles,
-                                        high_percentile=1 - percentiles,
-                                        mode='capping')
-        train_dataframes.append(df)
+    pca, pcdf, ratio = fl.fit_pca(df_train_labels, 2)
     
-    for te in test:
-        if te not in train:
-            df = merge[merge["label"] == te]
-            df, _ = fp.discard_outliers_by_iqr(df, low_percentile=percentiles,
-                                            high_percentile=1 - percentiles,
-                                            mode='capping')
-            test_dataframes.append(df)
+    # random split train test for test labels
+    df_test_labels = pd.DataFrame()
+    test_pcdf = pd.DataFrame()
+    if test:
+        df_test_labels = merge[merge["label"].isin(test)]
+        df_test_labels = fp.discard_outliers_by_iqr(df_test_labels, low_percentile=percentiles,
+                                                    high_percentile=1 - percentiles,
+                                                    mode='capping', metrics=iqr_metrics)
+        test_pcdf = fl.apply_pca(pca, df_test_labels)
     
-    pca, pcdf, ratios = fl.fit_pca(pd.concat(train_dataframes, ignore_index=True), n_components=n_component)
-    pcdf_applied = fl.apply_pca(pca, pd.concat(test_dataframes, ignore_index=True))
-    
-    fl.plot_pca(pd.concat([pcdf, pcdf_applied], ignore_index=True),
-                n_components=n_component,
+    fl.plot_pca(pd.concat([pcdf, test_pcdf], ignore_index=True), n_components=2,
                 show=True,
                 metrics=True,
-                ratios=[round(x, 2) for x in ratios])
+                savedir=savepath,
+                title=title,
+                ratios=[round(x, 2) for x in ratio],
+                dpi=300)
