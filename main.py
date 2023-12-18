29e4e2b058a56b8520993c2f34b4ec1d99843ed7
wlutz <lutz0willy@gmail.com>
2023-12-18 10:52:29 +0100

    update of fiiireflyyy

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ merge_vlp_spike = "example_data/merge vlp+ vlp- spike.csv"
 
 confusion(["-stachel ni t24", "-stachel inf t24"], ["-stachel stachel inf t24"], merge_path=merge_stachel)
 
-confusion(["Mock T24", "Inf T24"], ["Spike T24",], merge_path=merge_vlp_spike)
+# confusion(["Mock T24", "Inf T24"], ["Spike T24",], merge_path=merge_vlp_spike)
 
-pca(["Mock T24", "Inf T24"], ["Spike T24"], 2, merge_path=merge_vlp_spike)
+# pca(["Mock T24", "Inf T24"], ["Spike T24"], 2, merge_path=merge_vlp_spike)
